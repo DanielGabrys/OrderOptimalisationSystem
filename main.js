5ec3eff115dc659a211734285382d8b4c969a86c
DanielGabrys <daniello.gab@gmail.com>
2022-03-12 20:25:23 +0100

    base of callendar

--- main.js
+++ main.js
@@ -1,57 +1,36 @@
-class CustomSelect {
-    constructor(originalSelect) {
-        this.originalSelect = originalSelect;
-        this.customSelect = document.createElement("div");
-        this.customSelect.classList.add("select");
+const gridOptions = {
+    columnDefs: [
+        { field: 'athlete', minWidth: 150 },
+        { field: 'age', maxWidth: 90 },
+        { field: 'country', minWidth: 150 },
+        { field: 'year', maxWidth: 90 },
+        { field: 'date', minWidth: 150 },
+        { field: 'sport', minWidth: 150 },
+        { field: 'gold' },
+        { field: 'silver' },
+        { field: 'bronze' },
+        { field: 'total' },
+    ],
+    defaultColDef: {
+        flex: 1,
+        minWidth: 100,
+    },
+    rowSelection: 'single',
+    onSelectionChanged: onSelectionChanged,
+};
 
-        this.originalSelect.querySelectorAll("option").forEach((optionElement) => {
-            const itemElement = document.createElement("div");
-
-            itemElement.classList.add("select__item");
-            itemElement.textContent = optionElement.textContent;
-            this.customSelect.appendChild(itemElement);
-
-            if (optionElement.selected) {
-                this._select(itemElement);
-            }
-
-            itemElement.addEventListener("click", () => {
-                if (
-                    this.originalSelect.multiple &&
-                    itemElement.classList.contains("select__item--selected")
-                ) {
-                    this._deselect(itemElement);
-                } else {
-                    this._select(itemElement);
-                }
-            });
-        });
-
-        this.originalSelect.insertAdjacentElement("afterend", this.customSelect);
-        this.originalSelect.style.display = "none";
-    }
-
-    _select(itemElement) {
-        const index = Array.from(this.customSelect.children).indexOf(itemElement);
-
-        if (!this.originalSelect.multiple) {
-            this.customSelect.querySelectorAll(".select__item").forEach((el) => {
-                el.classList.remove("select__item--selected");
-            });
-        }
-
-        this.originalSelect.querySelectorAll("option")[index].selected = true;
-        itemElement.classList.add("select__item--selected");
-    }
-
-    _deselect(itemElement) {
-        const index = Array.from(this.customSelect.children).indexOf(itemElement);
-
-        this.originalSelect.querySelectorAll("option")[index].selected = false;
-        itemElement.classList.remove("select__item--selected");
-    }
+function onSelectionChanged() {
+    const selectedRows = gridOptions.api.getSelectedRows();
+    document.querySelector('#selectedRows').innerHTML =
+        selectedRows.length === 1 ? selectedRows[0].athlete : '';
 }
 
-document.querySelectorAll(".custom-select").forEach((selectElement) => {
-    new CustomSelect(selectElement);
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', () => {
+    const gridDiv = document.querySelector('#myGrid');
+    new agGrid.Grid(gridDiv, gridOptions);
+
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+        .then((response) => response.json())
+        .then((data) => gridOptions.api.setRowData(data));
 });
