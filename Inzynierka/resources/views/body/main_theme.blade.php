6b234addd06c57971bbeaf4b3e6353d2a8468870
DanielGabrys <daniello.gab@gmail.com>
2023-04-25 11:10:28 +0200

    refactoring

--- Inzynierka/resources/views/body/main_theme.blade.php
+++ Inzynierka/resources/views/body/main_theme.blade.php
@@ -8,15 +8,15 @@
     <meta name="author" content="" />
     <title>Dashboard - SB Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
-    <link href="{{asset('css/styles.css')}}" rel="stylesheet" />
+    <link href="{{asset('public/css/styles.css')}}" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
 
-    <script src="{{asset('Frontend/Js/Grid/BasicGrid.js')}}"></script>
-    <script src="{{asset('Frontend/Js/Grid/CreateGrid.js')}}"></script>
-    <script src="{{asset('Frontend/Js/Grid/ProductsGrid.js')}}"></script>
-    <script src="{{asset('Frontend/Js/Grid/DetailedGrid.js')}}"></script>
-    <script src="{{asset('Frontend/Js/Grid/ShowGrid.js')}}"></script>
-    <script src="{{asset('Frontend/Js/Grid/EditStructureGrid.js')}}"></script>
+    <script src="{{asset('public/Frontend/Js/Grid/BasicGrid.js')}}"></script>
+    <script src="{{asset('public/Frontend/Js/Grid/CreateGrid.js')}}"></script>
+    <script src="{{asset('public/Frontend/Js/Grid/ProductsGrid.js')}}"></script>
+    <script src="{{asset('public/Frontend/Js/Grid/DetailedGrid.js')}}"></script>
+    <script src="{{asset('public/Frontend/Js/Grid/ShowGrid.js')}}"></script>
+    <script src="{{asset('public/Frontend/Js/Grid/EditStructureGrid.js')}}"></script>
 
     <script src="{{asset('Frontend/Js/Dikstra/DikstraGrid.js')}}"></script>
 
